02ce8813c15c19b58d87b0bc7db7f6ff4642a165
LazyBear <jasonwhister@gmail.com>
2018-02-13 09:29:08 +0800

    Update

--- Python/SICP/2.3.py
+++ Python/SICP/2.3.py
@@ -86,3 +86,61 @@ for _ in range(3):
 
 
 # String
+
+# Interface conventions
+def fib(k):
+    """Compute the kth Fibonacci number."""
+    prev, curr = 1, 0  # curr is the first Fibonacci number.
+    for _ in range(k - 1):
+        prev, curr = curr, prev + curr
+    return curr
+
+
+def iseven(n):
+    return n % 2 == 0
+
+
+def sum_even_fibs(n):
+    """Sum the first n even Fibonacci numbers."""
+    return sum(filter(iseven, map(fib, range(1, n + 1))))
+
+
+print(sum_even_fibs(20))
+
+
+def first(s):
+    return s[0]
+
+
+def iscap(s):
+    return len(s) > 0 and s[0].isupper()
+
+
+def acronym(name):
+    """Return a tuple of the letters that form the acronym for name."""
+    return tuple(map(first, filter(iscap, name.split())))
+
+
+print(acronym('University of California Berkeley Undergraduate Graphics Group'))
+
+
+def acronym(name):
+    return tuple(w[0] for w in name.split() if iscap(w))
+
+
+def sum_even_fibs(n):
+    return sum(fib(k) for k in range(1, n + 1) if fib(k) % 2 == 0)
+
+
+from operator import mul
+from functools import reduce
+
+print(reduce(mul, (1, 2, 3, 4, 5)))
+
+
+def product_even_fibs(n):
+    """Return the product of the first n even Fibonacci numbers, except 0."""
+    return reduce(mul, filter(iseven, map(fib, range(2, n + 1))))
+
+
+print(product_even_fibs(20))
