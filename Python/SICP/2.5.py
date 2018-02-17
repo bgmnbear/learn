0a6d2bde6791f4e0cd1df24d5ba3e0bcb21ed45f
LazyBear <jasonwhister@gmail.com>
2018-02-17 09:56:32 +0800

    Update

--- Python/SICP/2.5.py
+++ Python/SICP/2.5.py
@@ -1,9 +1,32 @@
 # OOP
+
+
+class Account(object):
+    def __init__(self, account_holder):
+        self.balance = 0
+        self.holder = account_holder
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        return self.balance
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return 'Insufficient funds'
+        self.balance = self.balance - amount
+        return self.balance
+
+
 a = Account('Jim')
 print(a.holder)
 print(a.balance)
-print(a.deposit(15))
 
-print(a.withdraw(10))
-print(a.balance)
-print(a.withdraw(10))
+b = Account('Jack')
+b.balance = 200
+print([acc.balance for acc in (a, b)])
+
+tom_account = Account('Tom')
+print(tom_account.deposit(100))
+print(tom_account.withdraw(90))
+print(tom_account.withdraw(90))
+print(tom_account.holder)
