39c8d7633e363e6e8a713e63b3da6833132965f7
unknown <jasonwhister@gmail.com>
2018-03-03 23:15:01 +0800

    Update

--- Python/SICP/3.2.py
+++ Python/SICP/3.2.py
@@ -61,5 +61,20 @@ def memo(f):
     return memoized
 
 
+8
 fib = memo(fib)
 print(fib(40))
+
+
+# Example: Change zero
+def count_change(a, kinds=(50, 25, 10, 5, 1)):
+    """Return the number of ways to change amount a using coin kinds."""
+    if a == 0:
+        return 1
+    if a < 0 or len(kinds) == 0:
+        return 0
+    d = kinds[0]
+    return count_change(a, kinds[1:]) + count_change(a - d, kinds)
+
+
+print(count_change(100))
