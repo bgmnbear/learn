8aa1909afaf24eac8e1bba965bcddfdbcbd39a5f
unknown <jasonwhister@gmail.com>
2018-03-18 20:02:53 +0800

    First commit

---
+++ Kotlin/Kt07/src/main/java/net/println/kt07/Lambda.kt
@@ -0,0 +1,19 @@
+package net.println.kt07
+
+import rx.Observable
+import java.io.File
+import java.util.concurrent.Executors
+
+fun main(args: Array<String>) {
+    val text = File(ClassLoader.getSystemResource("input").path).readText()
+    Observable.from(text.toCharArray().asIterable()).filter { !it.isWhitespace() }.groupBy { it }.map {
+        o ->
+        o.count().subscribe {
+            println("${o.key} -> $it")
+        }
+    }.subscribe()
+    val worker = Executors.newCachedThreadPool()
+    worker.execute {
+        println("Hello")
+    }
+}
