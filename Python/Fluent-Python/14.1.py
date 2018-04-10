54de00fe7207b83003589f1bd8e6228cf0ea7db8
unknown <jasonwhister@gmail.com>
2018-04-10 22:56:50 +0800

    Update

---
+++ Python/Fluent-Python/14.1.py
@@ -0,0 +1,35 @@
+'''
+单词序列v1
+'''
+
+import re
+import reprlib
+
+RE_WORD = re.compile('\w+')
+
+
+class Sentence:
+    def __init__(self, text):
+        self.text = text
+        # 返回一个字符串列表、元素为正则所匹配到的非重叠匹配】
+        self.words = RE_WORD.findall(text)
+
+    def __getitem__(self, index):
+        # 返回指定索引上的单词
+        return self.words[index]
+
+    def __len__(self):
+        # 完善序列的协议，我们实现len方法
+        return len(self.words)
+
+    def __repr__(self):
+        # 该函数用于生成大型数据结构的简略字符串的表现形式
+        return 'Sentence(%s)' % reprlib.repr(self.text)
+
+
+# TEST 该类是否能够完成迭代
+
+s = Sentence('Ehco is a good Python coder')
+print(s)
+for word in s:
+    print(word)
