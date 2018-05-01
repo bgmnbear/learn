55664b09fd909f1731b56af012773da23d5eb6d1
unknown <jasonwhister@gmail.com>
2018-05-01 20:34:10 +0800

    Add web.py todo demo

--- Python/web.py/code.py
+++ Python/web.py/code.py
@@ -1,13 +1,29 @@
 import web
+from web.template import render
 
 urls = (
-    '/', 'index'
+    '/', 'index',
+    '/todo', 'todo',
 )
+
+db = web.database(dbn='postgres', user='username', pw='password', db='dbname')
 
 
 class index:
     def GET(self):
-        return "Hello, world!"
+        render = web.template.render('templates/')
+        return render.index()
+
+
+class todo:
+    def GET(self):
+        todos = db.select('todo')
+        return render.todo(todos)
+
+    def POST(self):
+        i = web.input()
+        n = db.insert('todo', title=i.title)
+        raise web.seeother('/todo')
 
 
 if __name__ == "__main__":
