d011228f6b442dcc815c5031d1e2e0b9a37e23e9
unknown <jasonwhister@gmail.com>
2018-05-13 22:40:58 +0800

    Update

--- Lisp/SICP/3b.scm
+++ Lisp/SICP/3b.scm
@@ -67,3 +67,32 @@
 ;: (deriv '(+ x 3) 'x)
 ;: (deriv '(* x y) 'x)
 ;: (deriv '(* (* x y) (+ x 3)) 'x)
+
+
+;; EXERCISE
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+        (else (error "unknown expression type -- DERIV" exp))))
+
+
+(define (deriv exp var)
+   (cond ((number? exp) 0)
+         ((variable? exp) (if (same-variable? exp var) 1 0))
+         (else ((get 'deriv (operator exp)) (operands exp)
+                                            var))))
+
+(define (operator exp) (car exp))
+
+(define (operands exp) (cdr exp))
+
+;: ((get (operator exp) 'deriv) (operands exp) var)
