731e2c90d2bb18ba7f397ec8a00582e9befe62e4
LazyBear <jasonwhister@gmail.com>
2018-07-24 23:18:27 +0800

    Add CSAPP

---
+++ C/CSAPP/Lab/01.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int thirdBits(void) {
+  int a = 0x49;
+  int b = (a << 9);
+  int c = b + a;
+  return (c << 18) + c; // Steps 4 and 5
+}
+
+int isTmin(int x) {
+   return !(x+x)&!!(x);
+}
+
+int isNotEqual(int x, int y)
+{
+	return(!!(x ^ y));
+}
+
+int anyOddBit(int x) {
+    return !!((x | (x >> 8) | (x >> 16) | (x >> 24)) & 0xaa);
+}
+
+int negate(int x) {
+  return ~x + 1;
+}
+
+int conditional(int x, int y, int z) {
+    /*
+     *if x!=0,mask=0x00000000,so y&~mask==y and z&mask==0
+     *if x==0,mask=0xffffffff,so y&~mask = y&0 =0; z&mask=z
+     */
+    int mask= ~!x+1; 
+    return (y & ~mask)|(z & mask);
+}
+
+int int main(int argc, char const *argv[]) {
+    return 0;
+}
