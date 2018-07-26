3d739d16eff3eae2ac42808052f8de81c3767b0c
LazyBear <jasonwhister@gmail.com>
2018-07-26 11:17:15 +0800

    Update

--- C/CSAPP/Lab/01.c
+++ C/CSAPP/Lab/01.c
@@ -30,10 +30,17 @@ int conditional(int x, int y, int z) {
      *if x!=0,mask=0x00000000,so y&~mask==y and z&mask==0
      *if x==0,mask=0xffffffff,so y&~mask = y&0 =0; z&mask=z
      */
-    int mask= ~!x+1; 
+    int mask= ~!x+1;
     return (y & ~mask)|(z & mask);
 }
 
-int int main(int argc, char const *argv[]) {
+int subOK(int x, int y) {
+    int res = ~y + 1 +x;
+    int sameSign = (x ^ y) >> 31;
+    int resSign = (res ^ x) >> 31;
+    return !(sameSign & resSign);
+}
+
+int main(int argc, char const *argv[]) {
     return 0;
 }
