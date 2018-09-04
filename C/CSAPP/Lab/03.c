1c74332d8209ef641f666033ff6749f44605db94
Jason Whister <jasonwhister@gmail.com>
2018-09-04 19:31:19 +0800

    Update

--- C/CSAPP/Lab/03.c
+++ C/CSAPP/Lab/03.c
@@ -22,3 +22,42 @@ void touch1() {
     validate(1);
     exit(0);
 }
+
+// phase 2
+void touch2(unsigned val){
+    vlevel = 2;
+    if (val == cookie){
+        printf("Touch2!: You called touch2(0x%.8x)\n", val);
+        validate(2);
+    } else {
+        printf("Misfire: You called touch2(0x%.8x)\n", val);
+        fail(2);
+    }
+    exit(0);
+}
+// 注入代码 p2.s
+mov $0x45374fee,%rdi # set my cookie as the first parameter
+pushq $0x401860
+ret
+// 转换对应机器码
+gcc -c p2.s
+objdump -d p2.o > p2.byte
+
+// phase 3
+int hexmatch(unsigned val, char *sval){
+    char cbuf[110];
+    char *s = cbuf + random() % 100;
+    sprintf(s, "%.8x", val);
+    return strncmp(sval, s, 9) == 0;
+}
+void touch3(char *sval){
+    vlevel = 3;
+    if (hexmatch(cookie, sval)){
+        printf("Touch3!: You called touch3(\"%s\")\n", sval);
+        validate(3);
+    } else {
+        printf("Misfire: You called touch3(\"%s\")\n", sval);
+        fail(3);
+    }
+    exit(0);
+}
