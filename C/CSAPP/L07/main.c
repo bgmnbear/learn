f9c8641b30e2e11c60f68f52a1ecfbf4209ee0e3
Jason Whister <jasonwhister@gmail.com>
2018-10-16 22:28:05 +0800

    Update

--- C/CSAPP/L07/main.c
+++ C/CSAPP/L07/main.c
@@ -1,3 +1,4 @@
+// Explicit allocate
 #include <stdio.h>
 #include <stdlib.h>
 void foo(int n) {
@@ -15,4 +16,61 @@ void foo(int n) {
         p[i] = i;
     /* Return allocated block to the heap */
     free(p);
+}
+
+
+// GC
+void foo() {
+    int *p = malloc(128);
+    return; /* p block is now garbage*/
+}
+
+
+// Memory Trick
+// Dereferencing Bad Pointers
+int val;
+...
+scanf("%d", val);
+
+// Reading Uninitialized Memory
+/* return y = Ax */
+int *matvec(int **A, int *x) {
+    int *y = malloc(N * sizeof(int));
+    int i, j;
+
+    for (i = 0; i < N; i++)
+        for (j = 0; j < N; j++)
+            y[i] += A[i][j] * x[j];
+    return y;
+}
+
+// Overwriting Memory
+int **p;
+p = malloc(N * sizeof(int));
+for (i = 0; i < N; i++)
+    p[i] = malloc(M * sizeof(int));
+//
+int **p;
+p = malloc(N * sizeof (int *));
+for (i = 0; i <= N; i++)
+    p[i] = malloc(M * sizeof(int));
+//
+char s[8];
+int i;
+gets(s); /* reads "123456789" from stdin */
+//
+int *search(int *p, int val) {
+    while (*p && *p != null)
+        p += sizeof(int);
+
+    return p;
+}
+//
+int *BinheapDelete(int **binheap, int *size) {
+    int *packet;
+    packet = binheap[0];
+    binheap[0] = binheap[*size - 1];
+    *size--;
+    Heapify(binheap, *size, 0);
+    return (packet);
 }
