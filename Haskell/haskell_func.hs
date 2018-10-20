14a523bf676b47044e9ee88e2b57afb69cfde747
Jason Whister <jasonwhister@gmail.com>
2018-10-20 19:27:35 +0800

    Update

--- Haskell/haskell_func.hs
+++ Haskell/haskell_func.hs
@@ -57,3 +57,13 @@ initials :: String -> String -> String
 initials firstname lastname = [f] ++ ". " ++ [l] ++ "."   
     where (f:_) = firstname   
           (l:_) = lastname  
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]   
+calcBmis xs = [bmi w h | (w, h)]
+    where bmi weight height = weight / height ^ 2  
+
+cylinder :: (RealFloat a) => a -> a -> a   
+cylinder r h =  
+    let sideArea = 2 * pi * r * h   
+        topArea = pi * r ^2   
+    in  sideArea + 2 * topArea 
