63d6a7aefbcdf75d770fe147ff602586fbeffe92
Jason Whister <jasonwhister@gmail.com>
2018-10-24 22:05:41 +0800

    Update

--- Haskell/higher_order_function.hs
+++ Haskell/higher_order_function.hs
@@ -8,3 +8,13 @@ zipWith f (x:xs) (y:ys) = (f x y) : zipWith f xs ys
 
 flip' :: (a -> b -> c) -> b -> a -> c   
 flip' f y x = f x y
+
+map :: (a -> b) -> [a] -> [b]
+map _ [] = []
+map f (x:xs) =  (f x):map f xs
+
+filter :: (a -> Bool) -> [a] -> [a]   
+filter _ [] = []   
+filter p (x:xs)    
+    | p x       = x : filter p xs   
+    | otherwise = filter p xs
