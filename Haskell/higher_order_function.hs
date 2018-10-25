264a8fa68d70dd13ec609fba872f5669e6a82c84
Jason Whister <jasonwhister@gmail.com>
2018-10-25 16:33:16 +0800

    Update

--- Haskell/higher_order_function.hs
+++ Haskell/higher_order_function.hs
@@ -22,3 +22,13 @@ filter p (x:xs)
 largestDivisible :: (Integral a) => a   
 largestDivisible = head (filter p [100000,99999..])   
     where p x = x `mod` 3829 == 0
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain num
+    | even num = num:chain (num `div` 2)
+    | odd num = odd:chain (num*3 + 1)
+
+numLongChains :: Int   
+numLongChains = length (filter isLong (map chain [1..100]))   
+    where isLong xs = length xs > 15
