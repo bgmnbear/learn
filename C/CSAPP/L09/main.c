f4a56dd7f6505d9837c158f7a05635cb211bd9e5
Jason Whister <jasonwhister@gmail.com>
2018-10-28 22:31:08 +0800

    Update

---
+++ C/CSAPP/L09/main.c
@@ -0,0 +1,26 @@
+// Process based
+void sigchld_handler(int sig){
+    while (waitpid(-1, 0, WNOHANG) > 0)
+        ;
+    return;
+    // Reap all zombie children
+}
+int main(int argc, char **argv) {
+    int listenfd, connfd;
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+
+    Signal(SIGCHLD, sigchld_handler);
+    listenfd = Open_listenfd(argv[1]);
+    while (1) {
+        clientlen = sizeof(struct sockaddr_storage);
+        connfd = Accept(listenfd, (SA *) &clientaddr, &clientlen);
+        if (Fork() == 0) {
+            Close(listenfd); // Child closes its listening socket
+            echo(connfd); // Child services client
+            Close(connfd); // Child closes connection with client
+            exit(0); // Child exits
+        }
+        Close(connfd); // Parent closes connected socket (important!)
+    }
+}
