7692f2187945a8c7ebb68e789558e5c6cece1e72
Jason Whister <jasonwhister@gmail.com>
2018-11-09 15:18:28 +0800

    Update

--- Haskell/module.hs
+++ Haskell/module.hs
@@ -2,3 +2,8 @@ import Data.List
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
+
+let stock = [(994.4,2008,9,1),(995.2,2008,9,2),(999.2,2008,9,3),(1001.4,2008,9,4),(998.3,2008,9,5)]   
+head (dropWhile (\(val,y,m,d) -> val 1000) stock)
+
+let (fw，rest) = span (/=' ') "This is a sentence" in "First word:" ++ fw ++ "，the rest:" ++ rest
