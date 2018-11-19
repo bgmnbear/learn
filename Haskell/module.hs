a445058a098a3a365909664bec44da61f0687dea
Jason Whister <jasonwhister@gmail.com>
2018-11-19 22:57:04 +0800

    Update

--- Haskell/module.hs
+++ Haskell/module.hs
@@ -51,3 +51,38 @@ findKey key = foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing
 
 fromList' :: (Ord k) => [(k,v)] -> Map.Map k v  
 fromList' = foldr (\(k,v) acc -> Map.insert k v acc) Map.empty
+
+
+phoneBookToMap :: (Ord k) => [(k，String)] -> Map.Map k String 
+phoneBookToMap xs = Map.fromListWith (\number1 number2 -> number1 ++ "，" ++ number2) xs
+
+
+module Geometry   
+( sphereVolume   
+，sphereArea   
+，cubeVolume   
+，cubeArea   
+，cuboidArea   
+，cuboidVolume   
+) where   
+
+sphereVolume :: Float -> Float   
+sphereVolume radius = (4.0 / 3.0) * pi * (radius ^ 3)   
+
+sphereArea :: Float -> Float   
+sphereArea radius = 4 * pi * (radius ^ 2)   
+
+cubeVolume :: Float -> Float   
+cubeVolume side = cuboidVolume side side side   
+
+cubeArea :: Float -> Float   
+cubeArea side = cuboidArea side side side   
+
+cuboidVolume :: Float -> Float -> Float -> Float   
+cuboidVolume a b c = rectangleArea a b * c   
+
+cuboidArea :: Float -> Float -> Float -> Float   
+cuboidArea a b c = rectangleArea a b * 2 + rectangleArea a c * 2 + rectangleArea c b * 2   
+
+rectangleArea :: Float -> Float -> Float   
+rectangleArea a b = a * b
