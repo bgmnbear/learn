3e22c7763ecfd66beba94b681d77ba71c64fd91d
Jason Whister <jasonwhister@gmail.com>
2018-11-24 23:48:44 +0800

    Update

--- Haskell/make_our_type.hs
+++ Haskell/make_our_type.hs
@@ -17,3 +17,18 @@ surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - 
 nudge :: Shape -> Float -> Float -> Shape   
 nudge (Circle (Point x y) r) a b = Circle (Point (x+a) (y+b)) r   
 nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))
+
+baseCircle :: Float -> Shape
+baseCircle r = Circle (Point 0 0) r
+
+baseRec :: Float -> Float -> Shape
+baseRec width height = Rectangle (Point 0 0) (Point width height)
+
+
+data Person = Person { firstName :: String   
+                     , lastName :: String   
+                     , age :: Int   
+                     , height :: Float   
+                     , phoneNumber :: String   
+                     , flavor :: String   
+                     } deriving (Show)
