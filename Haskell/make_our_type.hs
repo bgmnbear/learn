910bffde70816fcab54d2223ff2363f8ff13cf84
Jason Whister <jasonwhister@gmail.com>
2018-11-26 20:42:18 +0800

    Update

--- Haskell/make_our_type.hs
+++ Haskell/make_our_type.hs
@@ -32,3 +32,20 @@ data Person = Person { firstName :: String
                      , phoneNumber :: String   
                      , flavor :: String   
                      } deriving (Show)
+
+
+data Maybe a = Nothing | Just a
+
+tellCar :: Car -> String 
+tellCar (Car {company = c, model = m, year = y}) = "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
+
+tellCar :: (Show a) => Car String String a -> String   
+tellCar (Car {company = c, model = m, year = y}) = "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
+
+data Vector a = Vector a a a deriving (Show)     
+vplus :: (Num t) => Vector t -> Vector t -> Vector t   
+(Vector i j k) `vplus` (Vector l m n) = Vector (i+l) (j+m) (k+n)     
+vectMult :: (Num t) => Vector t -> t -> Vector t   
+(Vector i j k) `vectMult` m = Vector (i*m) (j*m) (k*m)     
+scalarMult :: (Num t) => Vector t -> Vector t -> t   
+(Vector i j k) `scalarMult` (Vector l m n) = i*l + j*m + k*n
