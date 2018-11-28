c42ec68f673de387e066619d96a2e90db8255ade
Jason Whister <jasonwhister@gmail.com>
2018-11-28 22:59:24 +0800

    Update

--- Haskell/make_our_type.hs
+++ Haskell/make_our_type.hs
@@ -49,3 +49,19 @@ vectMult :: (Num t) => Vector t -> t -> Vector t
 (Vector i j k) `vectMult` m = Vector (i*m) (j*m) (k*m)     
 scalarMult :: (Num t) => Vector t -> Vector t -> t   
 (Vector i j k) `scalarMult` (Vector l m n) = i*l + j*m + k*n
+
+
+data Person = Person { firstName :: String   
+                     , lastName :: String   
+                     , age :: Int   
+                     }
+
+data Person = Person { firstName :: String   
+                     , lastName :: String   
+                     , age :: Int   
+                     } deriving (Eq)   
+                     
+data Person = Person { firstName :: String   
+                     , lastName :: String   
+                     , age :: Int   
+                     } deriving (Eq, Show, Read)                     
