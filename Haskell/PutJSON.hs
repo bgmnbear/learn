c3a310070267e51bafaa1c1207d584634d695d42
Jason Whister <jasonwhister@gmail.com>
2018-12-12 23:39:21 +0800

    Bug fixes

--- Haskell/PutJSON.hs
+++ Haskell/PutJSON.hs
@@ -10,12 +10,14 @@ renderJValue (JBool True)  = "true"
 renderJValue (JBool False) = "false"
 renderJValue JNull         = "null"
 renderJValue (JObject o) = "{" ++ pairs o ++ "}"
-  where pairs [] = ""
+  where
+    pairs [] = ""
     pairs ps = intercalate ", " (map renderPair ps)
     renderPair (k,v)   = show k ++ ": " ++ renderJValue v
 renderJValue (JArray a) = "[" ++ values a ++ "]"
-  where values [] = ""
+  where
+    values [] = ""
     values vs = intercalate ", " (map renderJValue vs)
 
 putJValue :: JValue -> IO ()
-putJValue v = putStrLn (renderJValue v)    
+putJValue v = putStrLn (renderJValue v)
