e627a7fd6c923402884d40935076c3223d70819a
Jason Whister <jasonwhister@gmail.com>
2018-12-24 22:16:33 +0800

    Update

--- Go/go_qsort/qsort.go
+++ Go/go_qsort/qsort.go
@@ -1,10 +1,28 @@
 package go_qsort
 
 import "C"
-import "unsafe"
+import (
+	"sync"
+	"unsafe"
+)
 
-type CompareFunc C.qsort_cmp_func_t
+var go_qsort_compare_info struct {
+	fn func(a, b unsafe.Pointer) int
+	sync.Mutex
+}
 
-func Sort(base unsafe.Pointer, num, size int, cmp CompareFunc) {
-	C.qsort(base, C.size_t(num), C.size_t(size), C.qsort_cmp_func_t(cmp))
+//export _cgo_qsort_compare
+func _cgo_qsort_compare(a, b unsafe.Pointer) C.int {
+	return C.int(go_qsort_compare_info.fn(a, b))
+}
+
+func Sort(base unsafe.Pointer, num, size int, cmp func(a, b unsafe.Pointer) int) {
+	go_qsort_compare_info.Lock()
+	defer go_qsort_compare_info.Unlock()
+
+	go_qsort_compare_info.fn = cmp
+
+	C.qsort(base, C.size_t(num), C.size_t(size),
+		C.qsort_cmp_func_t(C._cgo_qsort_compare),
+	)
 }
