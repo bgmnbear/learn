0561b75311b6fb5ccebc675df623d0e689b192e2
Jason Whister <jasonwhister@gmail.com>
2018-12-26 23:20:44 +0800

    Update

---
+++ Go/go_rpc/hello_client.go
@@ -0,0 +1,23 @@
+package go_rpc
+
+import (
+	"log"
+	"net"
+	"net/rpc"
+)
+
+func main() {
+	rpc.RegisterName("HelloService", new(HelloService))
+
+	listener, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		log.Fatal("ListenTCP error:", err)
+	}
+
+	conn, err := listener.Accept()
+	if err != nil {
+		log.Fatal("Accept error:", err)
+	}
+
+	rpc.ServeConn(conn)
+}
