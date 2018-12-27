bbf549523e36f2dad52dc4b0170df4b847242555
Jason Whister <jasonwhister@gmail.com>
2018-12-28 00:04:23 +0800

    Update

--- Go/go_rpc/hello_client.go
+++ Go/go_rpc/hello_client.go
@@ -2,22 +2,36 @@ package go_rpc
 
 import (
 	"log"
-	"net"
 	"net/rpc"
 )
 
+type HelloServiceClient struct {
+	*rpc.Client
+}
+
+var _ HelloServiceInterface = (*HelloServiceClient)(nil)
+
+func DialHelloService(network, address string) (*HelloServiceClient, error) {
+	c, err := rpc.Dial(network, address)
+	if err != nil {
+		return nil, err
+	}
+	return &HelloServiceClient{Client: c}, nil
+}
+
+func (p *HelloServiceClient) Hello(request string, reply *string) error {
+	return p.Client.Call(HelloServiceName+".Hello", request, reply)
+}
+
 func main() {
-	rpc.RegisterName("HelloService", new(HelloService))
-
-	listener, err := net.Listen("tcp", ":1234")
+	client, err := DialHelloService("tcp", "localhost:1234")
 	if err != nil {
-		log.Fatal("ListenTCP error:", err)
+		log.Fatal("dialing:", err)
 	}
 
-	conn, err := listener.Accept()
+	var reply string
+	err = client.Hello("hello", &reply)
 	if err != nil {
-		log.Fatal("Accept error:", err)
+		log.Fatal(err)
 	}
-
-	rpc.ServeConn(conn)
 }
--- Go/go_rpc/hello_server.go
+++ Go/go_rpc/hello_server.go
@@ -6,6 +6,16 @@ import (
 	"net/rpc"
 )
 
+const HelloServiceName = "path/to/pkg.HelloService"
+
+type HelloServiceInterface = interface {
+	Hello(request string, reply *string) error
+}
+
+func RegisterHelloService(svc HelloServiceInterface) error {
+	return rpc.RegisterName(HelloServiceName, svc)
+}
+
 type HelloService struct {}
 
 func (p *HelloService) Hello(request string, reply *string) error {
@@ -14,17 +24,19 @@ func (p *HelloService) Hello(request string, reply *string) error {
 }
 
 func main() {
-	rpc.RegisterName("HelloService", new(HelloService))
+	RegisterHelloService(new(HelloService))
 
 	listener, err := net.Listen("tcp", ":1234")
 	if err != nil {
 		log.Fatal("ListenTCP error:", err)
 	}
 
-	conn, err := listener.Accept()
-	if err != nil {
-		log.Fatal("Accept error:", err)
-	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal("Accept error:", err)
+		}
 
-	rpc.ServeConn(conn)
+		go rpc.ServeConn(conn)
+	}
 }
